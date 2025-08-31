e8ed6b4b281bf9ae69eae654d2356fc022b6baf0
Matias Nicolas Gomez Osorio <mgomez22@alumnos.utalca.cl>
2025-08-31 19:01:10 -0400

    Update Ejecutora.java

--- src/Serializacion/Ejecutora.java
+++ src/Serializacion/Ejecutora.java
@@ -1,35 +1,39 @@
 import java.util.Date;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Ejecutora {
     public static void main(String[] args) {
-        // creacion usuarios, fechas y tareas
-        Usuario u1=new Usuario("Matias", "matias123@alumnos.utalca.cl", "contraseñaSEGURA");
-        Usuario u2=new Usuario("Carlos", "carloselmaspro@alumnos.utalca.cl", "TodoAlRojo");
+        // Creación de la base de datos
+        BaseDatos b1 = new BaseDatos();
+        
+        // Comentar desde aca para mostrar serializacion
+        // Creación usuarios, fechas y tareas
+        Usuario u1 = new Usuario("Matias", "matias123@alumnos.utalca.cl", "cotraseñaSEGURA");
+        Usuario u2 = new Usuario("Carlos", "carloselmaspro@alumnos.utalca.cl", "TodoAlRojo");
 
-        Date fecha_actual=new Date();
+        Date fecha_actual = new Date();
         Date fechaManana = new Date(System.currentTimeMillis() + 86400000); // +1 día
 
-        Tarea t1=new Tarea("Estudiar EDO", "Repasar la materia vista en clases", 150, fechaManana);
-        Tarea t2=new Tarea("Salir a trotar", "Salir a trotar por 20 minutos", 100, fecha_actual);
+        Tarea t1 = new Tarea("Estudiar EDO", "Repasar la materia vista en clases", 150, fechaManana);
+        Tarea t2 = new Tarea("Salir a trotar", "Salir a trotar por 20 minutos", 100, fecha_actual);
+        Tarea t3 = new Tarea("Hacer ejercicio", "30 minutos de gym", 80, fecha_actual);
 
-        //asignacion tareas a usuarios
+        // Asignación tareas a usuarios
         u1.agregarTarea(t1);
         u1.agregarTarea(t2);
+        u2.agregarTarea(t3);
 
-        /** 
-        System.out.println(u1.toString());
-        System.out.println(u1.getTareas().toString());
-        System.out.println(u2.toString());
-        **/
+        // Agregar usuarios a la base de datos
+        if(!b1.usuarioExistePorCorreo(u1.getCorreo_electronico())){
+            b1.agregarUsuario(u1);
+        }
+        if(!b1.usuarioExistePorCorreo(u2.getCorreo_electronico())){
+            b1.agregarUsuario(u2);
+        }
 
-        //serializacion
-        BaseDatos b1= new BaseDatos();
-        //b1.agregarUsuario(u1);
-        //b1.agregarUsuario(u2);
+        // Comentar hasta aca para mostrar serializacion
+        System.out.println("=== Base de datos ===");
         b1.imprimirTodosUsuarios();
-        b1.guardarBaseDatos();
+
         
     }
 }
