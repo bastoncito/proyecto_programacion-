e1b002fe2acd4a017f2e4b0f8edf08393d56b83a
bastoncito <breyes24@alumnos.utalca.cl>
2025-10-11 13:41:01 -0300

    Más cosas de manejo de excepciones

--- src/main/java/Michaelsoft_Binbows/controller/AdminController.java
+++ src/main/java/Michaelsoft_Binbows/controller/AdminController.java
@@ -468,7 +468,7 @@ public class AdminController {
             redirectAttributes.addAttribute("correo", correoUsuario);
             return "redirect:/admin";
 
-        } catch (TareaInvalidaException | RegistroInvalidoException e) {
+        } catch (TareaInvalidaException e) {
             // Si hay un error de validación (nombre duplicado, etc.)
             throw new AdminCrearTareaException(e.getMessage());
         }
