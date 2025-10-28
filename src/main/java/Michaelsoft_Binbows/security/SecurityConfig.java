d0c1204b29ba287851e4402fdff6d1f1172a0564
bastoncito <breyes24@alumnos.utalca.cl>
2025-10-28 20:59:36 -0300

    Editar SecurityConfig para que recursos estáticos sean accesibles

--- src/main/java/Michaelsoft_Binbows/security/SecurityConfig.java
+++ src/main/java/Michaelsoft_Binbows/security/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(
             auth ->
                 auth.requestMatchers(
-                        "/", "/**.css", "/**.png", "/**.jpg", "/login", "/register", "/api/**")
+                        "/", "/styles/**", "/images/**" , "/login", "/register", "/api/**")
                     .permitAll() // public endpoints
                     .requestMatchers("/admin/**")
                     .hasAnyAuthority("ROLE_ADMIN", "ROLE_MODERADOR")
